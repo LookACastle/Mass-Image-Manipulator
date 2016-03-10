aeaf05f00c248367430928596413578002423539
SirDancealot <mj@lpj-software.dk>
2016-03-10 16:43:27 +0100

    to_be_manipulated now gets value
    to_be_manipulated now gets the name of all the files we want to change
    as a list

--- MIM.py
+++ MIM.py
@@ -3,17 +3,22 @@ import Image
 import os
 
 def image_manipulation(changes):
-    if changes = 1:
-        # Function
-    elif changes = 2:
-        # Function
+    if changes == 1:
+        Function
+    elif changes == 2:
+		Function
     else:
         raise Exception("Program recieved unexpected value")
 
 
-path = input("Please input path to folder")
+path = input("Please input path to folder: ")
 print "For downscaling: 1"
 print "For image format conversion: 2"
 changes = input("Pick one option: ") #Add functionality so that we can say "or more" at the end.
 
-to_be_manipulated = os.listdir(path)
+to_be_manipulated = []
+
+for file in os.listdir(path):
+	if file.endswith(".txt"):
+		to_be_manipulated.append(file)
+print to_be_manipulated
