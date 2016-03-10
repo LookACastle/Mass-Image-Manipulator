1e65c268c624bdcd3115a0e36faa2ff9ed01feb1
LookACastle <thorkik@gmail.com>
2016-03-10 15:15:30 +0100

    Made a starting point
    It can't actually do anything yet.

--- MIM.py
+++ MIM.py
@@ -1,1 +1,19 @@
 # -*- coding: utf-8 -*-
+import Image
+import os
+
+def image_manipulation(changes):
+    if changes = 1:
+        # Function
+    elif changes = 2:
+        # Function
+    else:
+        raise Exception("Program recieved unexpected value")
+
+
+path = input("Please input path to folder")
+print "For downscaling: 1"
+print "For image format conversion: 2"
+changes = input("Pick one option: ") #Add functionality so that we can say "or more" at the end.
+
+to_be_manipulated = os.listdir(path)
