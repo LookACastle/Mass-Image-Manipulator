47aad714a4e2f8d6d7025d64e4f27515fbdc2f91
SirDancealot <mj@lpj-software.dk>
2016-03-11 08:34:28 +0100

    converted from python 2.2 to 3.5
    fixed syntax errors

--- MIM.py
+++ MIM.py
@@ -14,7 +14,7 @@ def image_manipulation(changes):
     if changes == 2:
         new_image_format = input("Please enter your new image format: ").upper.replace(".", None)
         if new_image_format not in supported_image_formats:
-            print "%s not supported, choose another image format." % (new_image_format)
+            print("%s not supported, choose another image format.") % (new_image_format)
             image_manipulation(changes)
         else:
             change_format(img, new_image_format)
@@ -26,15 +26,15 @@ def change_format(img, new_image_format):
     new_image.save("img", ".", "new_image_format".lower)
 
 # Variables
-path = raw_input("Please input path to folder: ")
-print "For downscaling: 1"
-print "For image format conversion: 2"
+path = input("Please input path to folder: ")
+print("For downscaling: 1")
+print("For image format conversion: 2")
 changes = input("Pick one option: ") #Add functionality so that we can say "or more" at the end.
 to_be_manipulated = []
 
 for file in os.listdir(path):
 	if file.endswith(".txt"): #NOTE NOTE NOTE NOTE NOTE Change .txt to a variable someday NOTE NOTE NOTE NOTE NOTE
 		to_be_manipulated.append(file)
-print to_be_manipulated
+print(to_be_manipulated)
 
 input("Finished")
