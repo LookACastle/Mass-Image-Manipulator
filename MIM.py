e219586fa612565015a6e7c9a79f077128e9a1cf
LookACastle <thorkik@gmail.com>
2016-03-11 13:27:09 +0100

    Did a lot of stuff
    - Tried implementing multicore support, will test later
    - Changed supported formats to pillow instead of PIL
    - Made 3 lists, read only supported formats, write only supported and
    supported.
    - Fixed some leftover python 2
    - Made input image format a variable
    - Made file conversion work

--- MIM.py
+++ MIM.py
@@ -1,40 +1,57 @@
 # -*- coding: utf-8 -*-
 from PIL import Image
+import multiprocessing as mp
 import os
 
 # Static variables
 supported_image_formats = [
-"BMP", "DIB", "DCX", "EPS", "PS", "GIF", "IM", "JPG", "JPE", "JPEG", "PCD", "PCX", "PDF", "PNG", "PBM", "PBM", "PGM", "PPM", "PSD", "TIF", "TIFF", "XBM", "XPM"
+"BMP", "EPS", "GIF", "ICNS", "IM", "JPG", "J2K", "MSP", "PCX", "PNG", "PPM", "SPIDER", "TIFF", "WEBP", "XBM", "XPM"
 ]
 
+read_only_image_formats = [
+"CUR", "DCX", "FLI", "FPX", "GBR", "GD", "ICO", "IMT", "IPTC", "NAA", "MCIDAS", "MIC", "MPO", "PCD", "PIXAR", "PSD", "SGI", "TGA", "WAL", "XPM"
+]
+
+write_only_image_formats = [
+"PALM", "PDF", "XVPICS"
+]
+
+pool = mp.Pool()
+
 #Functions
-def image_manipulation(changes):
-    #if changes == 1:
+def image_manipulation(changes, to_be_manipulated, input_image_format, new_image_format):
+    #if changes == "1":
         #Function
-    if changes == 2:
-        new_image_format = input("Please enter your new image format: ").upper.replace(".", None)
-        if new_image_format not in supported_image_formats:
-            print "%s not supported, choose another image format." % (new_image_format)
-            image_manipulation(changes)
+    if changes == "2":
+        if new_image_format not in supported_image_formats and new_image_format not in write_only_image_formats:
+            print(new_image_format, "not supported, choose another image format.")
+            print("I have support for these image formats: ", supported_image_formats, "And I have write only support for: ", write_only_image_formats)
+            image_manipulation(changes, to_be_manipulated)
         else:
-            change_format(img, new_image_format)
+            change_format(to_be_manipulated, new_image_format, input_image_format)
     else:
         raise Exception("Program recieved unexpected value")
 
-def change_format(img, new_image_format):
-    new_image = Image.open("img")
-    new_image.save("img", ".", "new_image_format".lower)
+def change_format(img, new_image_format, input_image_format):
+    new_image = Image.open(img)
+    new_image.save(img.replace(input_image_format, "." + new_image_format.lower()))
 
 # Variables
-path = raw_input("Please input path to folder: ")
-print "For downscaling: 1"
-print "For image format conversion: 2"
+path = input("Please input path to folder: ")
+print("For downscaling: 1")
+print("For image format conversion: 2")
 changes = input("Pick one option: ") #Add functionality so that we can say "or more" at the end.
+if changes == "2":
+    new_image_format = input("Please enter your new image format: ").upper()
 to_be_manipulated = []
+input_image_format = ".jpg" #Make it an input or something
 
 for file in os.listdir(path):
-	if file.endswith(".txt"): #NOTE NOTE NOTE NOTE NOTE Change .txt to a variable someday NOTE NOTE NOTE NOTE NOTE
+	if file.endswith(input_image_format):
 		to_be_manipulated.append(file)
-print to_be_manipulated
+
+for x in range(len(to_be_manipulated)):
+    image_manipulation(changes, path + "\\" + to_be_manipulated[x], input_image_format, new_image_format)
+
 
 input("Finished")
